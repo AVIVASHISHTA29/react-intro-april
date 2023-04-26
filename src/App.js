6dfbbb405f9d1f6e4710e17255feb261359a7c03
avivashishta29 <avivashishta@gmail.com>
2023-04-26 22:17:47 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Card from "./Card";
+import dog from "./assets/dog.jpeg";
 
 function App() {
   return (
     <>
-      <Card
+      {/* <Card
         name={"Shri Ram"}
         desc={"Shri Ram is a student at Acciojob in 2023 in F3"}
       />
@@ -15,7 +16,13 @@ function App() {
       <Card
         name={"Rodriguez"}
         desc={"Rodriguez is a student at Acciojob in 2023 in F3"}
+      /> */}
+      <button className="btn btn-primary">Click Me</button>
+      <img
+        width="400"
+        src="https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRPMKnq00NF_T7RusUNeLrSazRZM0S5O8_AOcw2iBTmYTxd3Q7uXf0sW41odpAKqSblKDMUMHGb8nZRo9g"
       />
+      <img width="400" src={dog} />
     </>
   );
 }
